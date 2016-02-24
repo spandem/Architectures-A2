bb1eea78fd586aae8b5fa786489e4dc43f4c5c3e
spandem <sashank.mitra@gmail.com>
2016-02-23 22:56:29 -0500

    SQL changes

---
+++ MySQLData/users.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS users.`users`;
+
+CREATE TABLE users.`users` (
+  `userid` int(10) NOT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `inventoryflag` varchar(5) DEFAULT NULL,
+  `orderflag` varchar(5) DEFAULT NULL,
+  `shippingflag` varchar(5) DEFAULT NULL  
+)
+
+INSERT INTO users.`users` values (1, 'inventoryuser', 'inventorypass', 'true', 'false', 'false');
+INSERT INTO users.`users` values (1, 'orderuser', 'orderpass', 'false', 'true', 'false');
+INSERT INTO users.`users` values (1, 'shippinguser', 'shippingpass', 'false', 'false', 'true');
+
+DROP TABLE IF EXISTS users.`loginactivity`;
+
+CREATE TABLE users.`loginactivity` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `userid` int(10) NOT NULL,
+  `timein` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `timeout` TIMESTAMP DEFAULT CURRENT_TIMESTAMP  
+)
+
